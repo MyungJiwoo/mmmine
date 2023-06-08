df90c62232be61c58c3ca446a7756868df341e68
MyungJiwoo <1206jiwoo@gmail.com>
2023-06-09 04:16:37 +0900

    price page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-let min = 10;
-min--;
-let max = 20;
+let priceFunc = (min, max, item) => {
+  let minPrice = min - 1;
+  let maxPrice = max;
+  let todayPrice = Math.floor(
+    Math.random() * (maxPrice - minPrice) + minPrice + 1
+  );
 
-let price = Math.floor(Math.random() * (max - min) + min + 1);
+  const itemTag = document.querySelector("." + item);
+  itemTag.innerText = `$ ${todayPrice}`;
+};
 
-console.log(price);
-
-const priceInfo = document.querySelector(".priceInfo");
-priceInfo.innerText = `오늘의 철 가격은 얼마일까요~~~~? ${price}`;
+priceFunc(10, 20, "stone");
+priceFunc(10, 20, "coal");
+priceFunc(10, 20, "iron");
+priceFunc(10, 20, "copper");
+priceFunc(10, 20, "gold");
+priceFunc(10, 20, "lapis_lazuli");
+priceFunc(100, 200, "diamond");
