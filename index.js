5991f5ab995a9e1873f0ea05fc3aad8de797799b
MyungJiwoo <1206jiwoo@gmail.com>
2023-06-09 02:37:40 +0900

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+let min = 10;
+min--;
+let max = 20;
+
+let price = Math.floor(Math.random() * (max - min) + min + 1);
+
+console.log(price);
+
+const priceInfo = document.querySelector(".priceInfo");
+priceInfo.innerText = `오늘의 가격은 ${price}`;
